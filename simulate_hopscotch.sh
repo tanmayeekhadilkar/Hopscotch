fa46702bd5b61a95cc465103388cd9b1a0dbeeaf
tanmayeekhadilkar <tmkhadilkar@ucdavis.edu>
2017-03-26 13:46:06 -0700

    Update simulate_hopscotch.sh

--- simulate_hopscotch.sh
+++ simulate_hopscotch.sh
@@ -1,3 +1,11 @@
+#!/bin/bash -login
+#SBATCH -D /home/tmkhadil/projects/Hopscotch/
+#SBATCH -o /home/tmkhadil/projects/Hopscotch/slurm-log/simulate_hopscotch-stdout-%j.txt
+#SBATCH -e /home/tmkhadil/projects/Hopscotch/slurm-log/simulate_hopscotch-stderr-%j.txt
+#SBATCH -J simulate_hopscotch
+#SBATCH -t 24:00:00
+set -e
+set -u
 
 ####################################################################
 ## Simulate hopscotch at random locations in the reference genome ##
@@ -6,11 +14,13 @@
 ##### pick a random region of the genome
 
 export CHR=$(( ( RANDOM % 10 )  + 1 ))   ## this is picking a random number between 1 and 10
-## chrsize is decided by following logic
-## there are size files with file name format <chromosomenumber>.CHR.SIZE for each chromosome which contain the sizes of each chromosome
-export CHRSIZE=$( cat $CHR.CHR.SIZE )
+
 ##!!!! this needs to be done! one way it could be done would be to make a file or array with each chromosome, and then make the max random number the chromosome length of the selected chromsome.
-POSITION=$(( ( RANDOM %  $CHRSIZE )  + 1 )) ## want to make this so that it can't be longer than the chromsome length
+
+## chrsize is decided by the following logic
+## there are size files with file name format <chromosomenumber>.chr.size for each chromosome which contain the sizes of each chromosome, i dont know the size of each chromosome but when I find that out I can put in those values into the files
+
+ export POSITION=$(( ( RANDOM % $(cat $CHR.chr.size) ) + 1 )) ## want to make this so that it can't be longer than the chromsome length
 
 ##### get two halves of the sequence, so we can put hopscotch in the middle, with a TSD
 
